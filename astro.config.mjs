243f49ebe941b4e1cb7a250c4d48cc767e890bd1
banegaserick@gmail.com <banegaserick@gmail.com>
2025-06-17 19:52:37 -0500

    delete  youtube

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,7 +34,6 @@ export default defineConfig({
         "@/shortcodes/Accordion",
         "@/shortcodes/Notice",
         "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
         "@/shortcodes/Tabs",
         "@/shortcodes/Tab",
       ],
